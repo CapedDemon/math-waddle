73e546f7f44099a6e1a6250df237142324806905
Shreejan <dolaishreejan@gmail.com>
2023-05-07 14:53:43 +0530

    Graph Successfully made for linear equation in two variable

--- pages/poly.js
+++ pages/poly.js
@@ -1,9 +1,147 @@
 import Link from "next/link";
-import { useState } from "react";
-import { Chart } from "chart.js";
+import { useState, useEffect, useRef } from "react";
+import Chart from "chart.js/auto";
+import math, { string } from "mathjs";
+
+// const range = math.range(-10, 10, 0.1).toArray();
+// const X = math.ones([range.length, range.length]).mul(range);
+// const Y = math.transpose(X);
 
 export default function Poly() {
   const [navbar, setNavbar] = useState(false);
+  const [value1, setValue1] = useState("");
+  const [value2, setValue2] = useState("");
+
+  // function to get coefficients and constant term of the
+  // linear equations
+  const getCoefficients = (eq) => {
+    // Parse the equation string to extract coefficients
+    const match = eq.match(
+      /([+-]?\d*)\s*x\s*([+-]?\d*)\s*y\s*=\s*([+-]?\d*)|([+-]?\d*)\s*x\s*\+\s*([+-]?\d*)\s*y\s*=\s*([+-]?\d*)/
+    );
+
+    // Return "failed" if the equation is invalid or does not have both x and y variables
+    if (
+      !match ||
+      (match[1] === "" && match[2] === "") ||
+      (match[4] === "" && match[5] === "")
+    ) {
+      return "failed";
+    }
+
+    // Extract the coefficients from the matched groups and convert them to numbers
+    const xCoeff = parseInt(match[1]) || parseInt(match[4]) || 1;
+    const yCoeff = parseInt(match[2]) || parseInt(match[5]) || 1;
+    const constantTerm = parseInt(match[3]) || parseInt(match[6]) || 0;
+
+    // Return the coefficients as an object
+    return {
+      xCoeff,
+      yCoeff,
+      constantTerm,
+    };
+  };
+
+  // main graph func
+  const LinearGraphFunc = (input1, input2, divId) => {
+    const result1 = getCoefficients(string(input1));
+    const result2 = getCoefficients(string(input2));
+    console.log(result1, result2);
+
+    const x1 = result1["xCoeff"];
+    const x2 = result2["xCoeff"];
+    const y1 = result1["yCoeff"];
+    const y2 = result2["yCoeff"];
+    const c1 = result1["constantTerm"];
+    const c2 = result2["constantTerm"];
+
+    // Check if a chart instance already exists
+    let chart = Chart.getChart(divId);
+
+    // Calculate the intersection point of the two lines
+    const xIntersect = (c2 * y1 - c1 * y2) / (x1 * y2 - x2 * y1);
+    const yIntersect = (c1 * x2 - c2 * x1) / (x1 * y2 - x2 * y1);
+
+    if (chart) {
+      // Update chart data and options
+      chart.data.datasets[0].label = `Equation 1: ${x1}x + ${y1}y = ${c1}`;
+      chart.data.datasets[0].data = [
+        (-c1 - x1 * -10) / y1,
+        (-c1 - x1 * 10) / y1,
+      ];
+      chart.data.datasets[1].label = `Equation 2: ${x2}x + ${y2}y = ${c2}`;
+      chart.data.datasets[1].data = [
+        (-c2 - x2 * -10) / y2,
+        (-c2 - x2 * 10) / y2,
+      ];
+      chart.data.datasets[2].data = [
+        {
+          x: xIntersect,
+          y: yIntersect,
+        },
+      ];
+      chart.update();
+    } else {
+      // Create a chart.js line chart configuration object
+      const chartConfig = {
+        type: "line",
+        data: {
+          labels: [-10, 10],
+          datasets: [
+            {
+              label: `Equation 1: ${x1}x + ${y1}y = ${c1}`,
+              data: [(-c1 - x1 * -10) / y1, (-c1 - x1 * 10) / y1],
+              fill: false,
+              borderColor: "red",
+              tension: 0.1,
+            },
+            {
+              label: `Equation 2: ${x2}x + ${y2}y = ${c2}`,
+              data: [(-c2 - x2 * -10) / y2, (-c2 - x2 * 10) / y2],
+              fill: false,
+              borderColor: "blue",
+              tension: 0.1,
+            },
+            {
+              label: "Intersection Point",
+              data: [
+                {
+                  x: xIntersect,
+                  y: yIntersect,
+                },
+              ],
+              pointRadius: 6,
+              pointBackgroundColor: "green",
+            },
+          ],
+        },
+        options: {
+          scales: {
+            x: {
+              type: "linear",
+              ticks: {
+                stepSize: 1,
+                max: 10,
+                min: -10,
+              },
+            },
+            y: {
+              type: "linear",
+              ticks: {
+                stepSize: 1,
+                max: 10,
+                min: -10,
+              },
+            },
+          },
+        },
+      };
+
+      // Get the canvas element and create the chart
+      const canvas = document.getElementById(divId);
+      new Chart(canvas, chartConfig);
+    }
+  };
 
   return (
     <div className="p-12 min-h-screen items-center flex-col">
@@ -87,6 +225,11 @@ export default function Poly() {
               <p>First equation (x+y=10) :- </p>
               <input
                 type="text"
+                id="input1"
+                value={value1}
+                onChange={(e) => {
+                  setValue1(e.currentTarget.value);
+                }}
                 className="mx-2 dark:bg-black border-2 border-black dark:border-white border-solid"
               />
             </div>
@@ -95,11 +238,21 @@ export default function Poly() {
               <p>Second equation (x+2y=20) :- </p>
               <input
                 type="text"
+                id="input2"
+                value={value2}
+                onChange={(e) => {
+                  setValue2(e.currentTarget.value);
+                }}
                 className="mx-2 dark:bg-black border-2 border-black dark:border-white border-solid"
               />
             </div>
-
-            
+            <br />
+            <button onClick={(e) => LinearGraphFunc(value1, value2, "graph")}>
+              Submit
+            </button>
+          </div>
+          <div>
+            <canvas id="graph" width="400" height="400"></canvas>
           </div>
         </div>
       </div>
